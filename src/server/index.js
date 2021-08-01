0463c4a687bf3ef9630e626e49317a73ebe7a61a
David C. Page <evildavo@gmail.com>
2021-08-01 14:52:39 +1000

    feat: Use HTML Webpack plugin

--- src/server/index.js
+++ src/server/index.js
@@ -3,10 +3,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, '../../dist/index.html')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/views/index.html'));
+    res.sendFile(path.join(__dirname, '../../dist/index.html'));
 });
 
 const PORT = 8080;
