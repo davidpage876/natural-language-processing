bc05ce16af1f5d685c2d258b57b971e88f02a063
David C. Page <evildavo@gmail.com>
2021-08-01 13:40:55 +1000

    feat: Set up node server configuration

---
+++ src/server/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/views/index.html'));
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
