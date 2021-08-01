e62b5191fc97f07d09757db53b31881d18e1adbf
David C. Page <evildavo@gmail.com>
2021-08-02 08:19:23 +1000

    feat: Retrieve API key from environment variable

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,11 @@
+
+// Retrieve Meaning Cloud API key from environment.
+// Expects "API_KEY=********************************".
+const dotenv = require('dotenv');
+dotenv.config();
+const apiKey = process.env.API_KEY;
+
+// Set up express server.
 const path = require('path');
 const express = require('express');
 
@@ -5,10 +13,12 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../../dist')));
 
+// Retrieve HTML on root get route.
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../dist/index.html'));
 });
 
+// Start production server on port 8080.
 const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
