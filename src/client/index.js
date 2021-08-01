16149ba132f8a81a70b3559fdbacf4394d2e14c6
David C. Page <evildavo@gmail.com>
2021-08-01 15:27:19 +1000

    feat: Switch to using SASS for styles

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,13 @@
 console.log("entry point started");
 
+// Scripts
 import { handleSubmit } from './js/handleForm.js';
+
+// Styles
+import './styles/normalize.scss';
+import './styles/base.scss';
+import './styles/header.scss';
+import './styles/footer.scss';
+import './styles/form.scss';
+import './styles/result.scss';
+
