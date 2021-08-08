a77534ad370d94af9859a902ed4255ff637b46f4
David C. Page <evildavo@gmail.com>
2021-08-08 22:18:07 +1000

    feat: Combine dev and production configurations into one common config
    Avoids repetition (DRY principle)

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const { merge } = require('webpack-merge');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+const commonConfig = {
+    entry: './src/client/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html'
+        }),
+        new CleanWebpackPlugin(),
+    ]
+};
+
+const productionConfig = {
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ]
+};
+
+const developmentConfig = {
+    mode: 'development',
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }
+        ]
+    }
+};
+
+module.exports = (env, args) => {
+  switch(args.mode) {
+    case 'development':
+      return merge(commonConfig, developmentConfig);
+    case 'production':
+      return merge(commonConfig, productionConfig);
+    default:
+      throw new Error('No matching configuration was found!');
+  }
+}
